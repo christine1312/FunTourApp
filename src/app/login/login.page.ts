be39af67e48c2529c200ca6dd60dda79d84d067b
Elina Olsson <elinao@student.chalmers.se>
2020-04-03 17:17:34 +0200

    Skeleton login page

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  imgfile="assets/tourists.jpg";
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.router.navigate(['/folder/Inbox'])
+  }
+
+}
