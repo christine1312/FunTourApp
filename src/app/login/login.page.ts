85777f248abb79d6b7df69c52020c0341af19ba9
Elina Olsson <elinao@student.chalmers.se>
2020-04-23 13:09:24 +0200

    Normal login works

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -10,15 +10,37 @@ import * as firebase from 'Firebase';
 export class LoginPage implements OnInit {
 
   imgfile="assets/tourists.jpg";
+  db = firebase.firestore();
+  email;
+  password;
+
 
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-  login() {
-    this.router.navigate(['/folder/MyPlan'])
+  login() {    
+    var self = this;
+
+    firebase.auth().signInWithEmailAndPassword(this.email, this.password).catch(function(error){
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else if (errorCode === 'auth/user-not-found') {
+        alert("User does not exist.");
+      }
+    }
+    ).then(function(result){
+      console.log(result)
+      if(result != undefined)
+        self.router.navigate(['/folder/MyPlan'])
+      })
   }
+
 
   loginGoogle() {
     var self = this;
@@ -38,6 +60,7 @@ export class LoginPage implements OnInit {
   loginFacebook() {
     var self = this;
     console.log("login facebook")
+    console.log("Need to create Facebook app before this can work properly")
     var provider = new firebase.auth.FacebookAuthProvider();
     provider.addScope('user_birthday');
     firebase.auth().signInWithPopup(provider).then(function(result){
