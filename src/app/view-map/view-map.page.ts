64df7e292d5cbc07f4e90aa99cb0e28ebbaee295
Christine <christine.burris@avolanta.com>
2020-04-29 23:18:49 -0400

    added geolocation

--- src/app/view-map/view-map.page.ts
+++ src/app/view-map/view-map.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild, AfterContentInit } from '@angular/core';
+import { Component, NgZone, OnInit, ViewChild, AfterContentInit } from '@angular/core';
+import { Geolocation, GeolocationOptions, Geoposition } from '@ionic-native/geolocation/ngx';
 import { Router } from '@angular/router';
+import { AttractionService } from '../attraction.service';
 import * as firebase from 'Firebase';
 import { TripsService } from '../trips.service';
 
@@ -11,21 +13,50 @@ declare var google;
   styleUrls: ['./view-map.page.scss'],
 })
 export class ViewMapPage implements OnInit, AfterContentInit {
-  map;
-  @ViewChild('mapElement', {static:true}) mapElement;
-  constructor(private router:Router, private tripsService: TripsService) { }
+  currentLat: any;
+  currentLng: any;
 
-  ngAfterContentInit(): void {
-    this.map = new google.maps.Map (
-      this.mapElement.nativeElement,
-      {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-      }
-    )
+  options: GeolocationOptions;
+  currentPos: Geoposition;
+  places: Array<any>;
+
+  @ViewChild('mapElement', {static:true}) mapElement;
+  map: any;
+
+  constructor(
+    public zone: NgZone,
+    private router:Router,
+    private geolocation: Geolocation,
+    public attractionService: AttractionService,
+    public tripsService: TripsService
+  ) { }
+
+  ngAfterContentInit(): void {}
+
+  ngOnInit() { 
+    this.loadMap();
   }
 
-  ngOnInit() {
+  loadMap() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.currentLat = resp.coords.latitude;
+      this.currentLng = resp.coords.longitude;
+      this.map = new google.maps.Map(this.mapElement.nativeElement, {
+        center: {lat: 38, lng: -99},
+        zoom: 3
+      });
+
+      let location = new google.maps.LatLng(this.currentLat, this.currentLng);
+
+      let marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'You are here'
+      });
+      marker.setPosition(location);
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
   }
 
   logout() {
