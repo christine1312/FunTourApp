539e7e3e1e6fb30526f6e331d5e9d144f97aff45
Christine <christine.burris@avolanta.com>
2020-04-30 02:03:29 -0400

    fixed login - got rid of hard-coded login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as firebase from 'Firebase';
+import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TestBed } from '@angular/core/testing';
 import { TripsService } from '../trips.service';
 
@@ -11,29 +13,46 @@ import { TripsService } from '../trips.service';
 })
 export class LoginPage implements OnInit {
 
+  loginForm: FormGroup;
   imgfile="assets/tourists.jpg";
   db = firebase.firestore();
-  email = "hi@test.com";
-  password = "secret";
 
 
-  constructor(private router: Router, private tripsService: TripsService) { }
+  constructor(
+    private router: Router, 
+    private tripsService: TripsService,
+    public formBuilder: FormBuilder  
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
   }
 
-  login() {    
+  login(item) {    
     var self = this;
+    var email=item.email;
+    var password=item.password;
+    const alert = document.createElement('ion-alert');
 
-    firebase.auth().signInWithEmailAndPassword(this.email, this.password).catch(function(error){
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
       var errorCode = error.code;
-      var errorMessage = error.message;
       console.log(errorCode);
 
       if (errorCode === 'auth/wrong-password') {
-        alert('Wrong password.');
+        alert.header = 'Alert';
+        alert.message = 'Wrong password';
+        alert.buttons = ['Okay'];
+        document.body.appendChild(alert);
+        return alert.present();
       } else if (errorCode === 'auth/user-not-found') {
-        alert("User does not exist.");
+        alert.header = 'Alert';
+        alert.message = 'No account associated with this email';
+        alert.buttons = ['Okay'];
+        document.body.appendChild(alert);
+        return alert.present();
       }
     }
     ).then(function(result){
@@ -44,7 +63,6 @@ export class LoginPage implements OnInit {
       }
       })
   }
-
 
   loginGoogle() {
     var self = this;
